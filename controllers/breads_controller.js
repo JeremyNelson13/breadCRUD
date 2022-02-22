d1da5847e890be9cc1d2087644fac72176f28eee
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-02-21 20:24:44 -0500

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const req = require('express/lib/request')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
@@ -18,6 +19,13 @@ breads.get('/new', (req,res) => {
     res.render('new')
 })
 
+//EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
+})
 //SHOW
 breads.get('/:arrayIndex', (req,res) => {
     if(Bread[req.params.arrayIndex]) {
@@ -44,6 +52,16 @@ breads.post('/', (req,res) => {
     res.redirect('/breads')
 })
 
+//UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = 'true'
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
+})
 //DELETE
 breads.delete('/:indexArray', (req,res) => {
     Bread.splice(req.params.indexArray, 1)
