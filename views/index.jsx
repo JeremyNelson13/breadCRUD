44c8624f45370b9da9c8586e9bc07748132de329
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-02-21 20:01:01 -0500

    adding create and new routes, adding navigation

--- views/index.jsx
+++ views/index.jsx
@@ -19,6 +19,12 @@ function Index ({breads, title}) {
                     })
                 }
             </ul>
+            <div className='newButton'>
+                <a href='/breads/new'><button>Add a New Bread</button></a>
+            </div>
+            <div>
+                <a href='/breads'><button>Return to Index</button></a>
+            </div>
         </Default>
     )
 }
